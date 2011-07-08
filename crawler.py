4c6276f0e78bd3c3769bb85fdaa34d9bc155dd59
rozuur <rozuur@gmail.com>
2011-07-08 20:31:53 +0530

    finding links and mail addresses is added

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import urllib2
+import os
 from BeautifulSoup import BeautifulSoup
+import collections
 
 class Crawler(object):
     def __init__(self, url):
@@ -7,9 +9,30 @@ class Crawler(object):
         page = urllib2.urlopen(url)
         self._soup = BeautifulSoup(page)
 
+    def is_mailid(self, url):
+        return url.upper().startswith('MAILTO:')
+
     def fetch_links(self):
-        print self._soup.findAll('a')
+        links = []
+        mailids = []
+        for node in self._soup.findAll('a'):
+            relurl = node['href']
+            if self.is_mailid(relurl):
+                print relurl
+                mailids.append(relurl)
+                print relurl
+            else:
+                links.append(os.path.join(self.url,relurl))
+        return  links, mailids
+
+    def fetch_all_links(self):
+        urls = collections.defaultdict(1)
+        urls.add(self.url)
+        return urls
+        
+        
 
 if __name__ == '__main__':
     crawler = Crawler('http://www.innopark.in/')
     print crawler.fetch_links()
+
