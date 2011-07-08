9710ace3b789b50e0edd30fd31d556a788f3fd56
rozuur <rozuur@gmail.com>
2011-07-08 19:58:00 +0530

    added a web crawler which just prints the link provided

---
+++ crawler.py
@@ -0,0 +1,15 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+class Crawler(object):
+    def __init__(self, url):
+        self.url = url
+        page = urllib2.urlopen(url)
+        self._soup = BeautifulSoup(page)
+
+    def fetch_links(self):
+        print self._soup.prettify()
+
+if __name__ == '__main__':
+    crawler = Crawler('http://www.innopark.in/')
+    print crawler.fetch_links()
