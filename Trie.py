6a4645844bc224db3b6fcfb4c953a5ac07d6c035
rozuur <rozuur@gmail.com>
2011-07-09 18:36:10 +0530

    display of trie is implemented

--- Trie.py
+++ Trie.py
@@ -36,15 +36,58 @@ class Trie:
                 return False
         return current.isWord
 
-    def display(self):
-        print 'root = ', self.root
-        for c in self.root.children:
-            print c, self.root.children[c]
+    def charfmt(self, c):
+        return '--[{0}]'.format(c)
 
+    def _display(self, node, depth, fmt):
+        childs = node.children.keys()
+        if not childs: 
+            return
+        for c in childs[:-1]:
+            print fmt + '|' +  self.charfmt(c)
+            self._display(node.children[c], 0, fmt + '|   ')
+        print fmt + '+'  + self.charfmt(childs[-1])
+        self._display(node.children[childs[-1]], 0, fmt + '   ')
+
+
+    def display(self):
+        print '[-]'
+        self._display(self.root, 0, ' ')
+            
+
+"""
+[-]
+ |--[A]
+ |--[T]
+ |   |--[E]
+ |       |--[A]
+ |       |   |--[A]
+ |       |--[D]
+ |       |--[N]
+ |--[I]
+     |--[N]
+         |--[N]
+"""
+
+"""
+[-]
+ |--[A]
+ |--[T]
+ |   +--[E]
+ |       |--[A]
+ |       |   +--[A]
+ |       |--[D]
+ |       +--[N]
+ +--[I]
+     +--[N]
+         +--[N]
+"""
 
 if __name__ == '__main__':
     tr = Trie()
-    for word in ["A", "to", "tea", "ted", "ten", "i", "in",  "inn"]:
+    words = ["A", "to", "tea", "ted", "ten", "i", "in",  "inn"]
+    #words = ["to", "tea", "ted", "ten",]
+    for word in words:
         tr.insert(word)
     tr.display()
     
